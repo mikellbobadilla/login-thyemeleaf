17c876a53712458580af3891d8b74f8e9b8a65c4
sting <bobadillasting@gmail.com>
2024-06-30 19:29:29 -0300

    Refactor create account form and create account

--- src/main/java/ar/mikellbobadilla/app/account/RegisterController.java
+++ src/main/java/ar/mikellbobadilla/app/account/RegisterController.java
@@ -1,9 +1,16 @@
 package ar.mikellbobadilla.app.account;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/accounts")
 public class RegisterController {
 
     private final AccountService service;
@@ -12,9 +19,21 @@ public class RegisterController {
         this.service = service;
     }
 
-    @GetMapping("/register")
-    String registerPage() throws AccountException {
-        service.createAccount("sting", "holamundo", Role.ADMIN);
+    @GetMapping("/create")
+    String registerPage(Model model) throws AccountException {
+        model.addAttribute("accountRequest", new AccountRequest());
         return "register";
+    }
+
+    @PostMapping("/create")
+    String createAccount(@Valid @ModelAttribute AccountRequest request, BindingResult result, Model model) {
+        try {
+            if (result.hasErrors()) return "register";
+            service.createAccount(request);
+            return "redirect:/";
+        } catch (AccountException exc) {
+            result.rejectValue(null, "error.accountRequest", exc.getMessage());
+            return "register";
+        }
     }
 }
